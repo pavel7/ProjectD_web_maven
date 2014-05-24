11a17b43d475de08da3aa73bf6a8e8658ab5caad
Cherepanov55 <kvaka64@ya.ru>
2014-05-24 13:54:38 +0700

    1) Realization login servlet

--- src/main/java/com/omsu/cherepanov/servlets/LoginServlet.java
+++ src/main/java/com/omsu/cherepanov/servlets/LoginServlet.java
@@ -1,9 +1,13 @@
 package com.omsu.cherepanov.servlets;
 
+import com.omsu.cherepanov.hibernate.UserDAO;
+import com.omsu.cherepanov.users.UserBean;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 /**
@@ -12,6 +16,30 @@ import java.io.IOException;
 public class LoginServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+        try {
+            if ((req.getParameter("login").length() <= 20) && (req.getParameter("password").length() <= 20)) {
+                UserDAO userDAO = new UserDAO();
+                UserBean user = new UserBean();
+                user.setUsername(req.getParameter("login"));
+                user.setPassword(req.getParameter("password"));
+                user = userDAO.login(user);
+                if (user.isValid()) {
+
+                    HttpSession session = req.getSession(true);
+                    session.setAttribute("currentSessionUser", user);
+                    resp.sendRedirect("main.jsp");
+                } else
+                    resp.sendRedirect("index.jsp");
+            }
+            else{
+                resp.sendRedirect("index.jsp");
+            }
+        }
+
+
+        catch (Throwable theException)
+        {
+            System.out.println(theException);
+        }
     }
 }
